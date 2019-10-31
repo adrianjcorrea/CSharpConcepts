27f15df51a93aff6bd57662c2f7dda5c967eb4d7
Jose Adrián Correa Méndez <jcorrea@TELEPROCORP.COM.MX>
2019-10-31 11:40:43 -0600

    RadioButton Controls Practice

---
+++ CSharpTutor/CSharpTutor/RadioButtonControl.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RadioButtonControl.aspx.cs" Inherits="CSharpTutor.RadioButtonControl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-family: Arial">
+        <fieldset style="width:300px">
+            <legend><b>Gender</b></legend>
+            <asp:RadioButton ID="MaleRadioButton" Text="Male" runat="server" GroupName="GenderGroup" OnCheckedChanged="MaleRadioButton_CheckedChanged" TextAlign="Left" />
+            <asp:RadioButton ID="FemaleRadioButton" Text="Female" runat="server" GroupName="GenderGroup" OnCheckedChanged="FemaleRadioButton_CheckedChanged" TextAlign="Left" />
+            <asp:RadioButton ID="UnknownRadioButton" Text="Unknown" runat="server" GroupName="GenderGroup" OnCheckedChanged="UnknownRadioButton_CheckedChanged" TextAlign="Left" />
+        </fieldset>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CSharpTutor/CSharpTutor/RadioButtonControl.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSharpTutor
+{
+    public partial class RadioButtonControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(MaleRadioButton.Checked)
+            {
+                Response.Write("You are a " + MaleRadioButton.Text + "<br />");
+            }
+
+            if (FemaleRadioButton.Checked)
+            {
+                Response.Write("You are a " + FemaleRadioButton.Text + "<br />");
+
+            }
+
+            if (UnknownRadioButton.Checked)
+            {
+                Response.Write("You are " + UnknownRadioButton.Text + " gender" + "<br />");
+
+            }
+        }
+
+        protected void UnknownRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("Unknown Gender Button Selected.");
+        }
+
+        protected void FemaleRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("Female Button Selected.");
+        }
+
+        protected void MaleRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("Male Button Selected.");
+        }
+    }
+}
